0ebeadd7ebea8c9b3caa648c2b83f6c14d58fbbb
Tomas Pazout <tom.pazout@gmail.com>
2025-06-11 20:03:58 +0200

    2. Smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const Token = await ethers.getContractFactory("Token");
+    // The `getContractFactory` function is used to get a contract factory for the Token contract.
+    // The `Token` contract is assumed to be defined in the contracts directory.
+    const token = await Token.deploy();
+    // The `deploy` function is used to deploy a new contract instance.
+    // The `deploy` function returns a promise that resolves to the deployed contract instance.
+    await token.deployed();
+    // The `deployed` function is used to wait for the contract to be deployed.
+    console.log(`Token deployed to: ${token.address}`);
+    // The `console.log` function is used to print the address of the deployed contract.
+    // The `address` property of the contract instance contains the address of the deployed contract.
+    // The `address` property is a string that contains the address of the deployed contract.
+    // The `address` property is used to interact with the deployed contract.
+    // The `address` property is used to get the address of the deployed contract.
+    // The `address` property is used to get the address of the deployed contract instance.
+}
+
+// The `console.log` function is used to print the address of the deployed contract.
+// The `main` function is an asynchronous function that deploys the Token contract.
+// The `main` function is the entry point of the script.
+// The `ethers` object is used to interact with the Ethereum blockchain.
+// The `ethers` object is provided by the Hardhat environment and is used to interact with the Ethereum blockchain.
+
+
+main()
+    .then(() => process.exit(0))
+    // The `then` method is used to handle the promise returned by the `main` function.
+    // The `process.exit(0)` method is used to exit the process with a success code.
+    // The `catch` method is used to handle any errors that occur during the execution of the `main` function.
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
+// The `catch` method is used to handle any errors that occur during the execution of the `main` function.
+// The `process.exit(1)` method is used to exit the process with an error code.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,21 +8,31 @@ const hre = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Lock = await hre.ethers.getContractFactory("Lock"); 
+  // importing the Lock contract
+  // The contract factory is an abstraction used to deploy new smart contracts,
+  // so Lock here is a factory for instances of our Lock contract.
+  // We can also use `hre.ethers.getContractAt` to get an instance of a deployed contract.
+  // The `getContractFactory` function is used to get a contract factory for the Lock contract.
+  // The `getContractAt` function is used to get an instance of a deployed contract.
+  // The `getContractFactory` function returns a promise that resolves to a contract factory.
+  // The `getContractAt` function returns a promise that resolves to a contract instance.
+  // The `getContractFactory` function takes the contract name as a parameter.
+  // The `getContractAt` function takes the contract name and address as parameters.
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount }); 
+  // deploying the contract
+  // The `deploy` function is used to deploy a new contract instance.
+  // The `deploy` function takes the constructor arguments as parameters.
+  // The `deploy` function returns a promise that resolves to the deployed contract instance.
 
-  await lock.waitForDeployment();
+  await lock.deployed();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
